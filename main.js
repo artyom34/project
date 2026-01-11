ca4e12e40379aacdf9732fb750ce56aa6000729e
artem34 <art.cheremisin@gmail.com>
2026-01-11 20:53:49 +0200

    used forEach to refactor the code

--- main.js
+++ main.js
@@ -13,33 +13,23 @@ const rootElement = document.getElementById("root");
 const titleElement = document.createElement("h1");
 titleElement.append("Music player");
 
+rootElement.append(titleElement);
+
 const listElements = document.createElement("ul");
 
-const trackElement1 = document.createElement("li");
-const trackTitleElement1 = document.createElement("div");
-trackTitleElement1.append(tracks[0].title);
+tracks.forEach((track) => {
+  const trackElements = document.createElement("li");
+  const trackTitleElement = document.createElement("div");
+  trackTitleElement.append(track.title);
 
-const audioElement1 = document.createElement("audio");
-audioElement1.controls = true;
-audioElement1.src = tracks[0].url;
+  trackElements.append(trackTitleElement);
 
-trackElement1.append(trackTitleElement1);
-trackElement1.append(audioElement1);
+  const audioElement = document.createElement("audio");
+  audioElement.controls = true;
+  audioElement.src = track.url;
+  trackElements.append(audioElement);
 
-listElements.append(trackElement1);
+  listElements.append(trackElements);
+});
 
-const trackElement2 = document.createElement("li");
-const trackTitleElement2 = document.createElement("div");
-trackTitleElement2.append(tracks[1].title);
-
-const audioElement2 = document.createElement("audio");
-audioElement2.controls = true;
-audioElement2.src = tracks[1].url;
-
-trackElement2.append(trackTitleElement2);
-trackElement2.append(audioElement2);
-
-listElements.append(trackElement2);
-
-rootElement.append(titleElement);
 rootElement.append(listElements);
