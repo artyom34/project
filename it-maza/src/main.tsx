2abace5f5e1482ec8fda7a6e93a590985a2f96d8
artem34 <art.cheremisin@gmail.com>
2026-01-12 18:57:04 +0200

    refactoring application for myself

--- it-maza/src/main.tsx
+++ it-maza/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+const newCreateRoot = createRoot(rootElement!);
+
+newCreateRoot.render(<App />);
