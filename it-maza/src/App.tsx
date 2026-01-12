e26085c520fc8895e1408fd7aa4bdbb1f03deddd
artem34 <art.cheremisin@gmail.com>
2026-01-12 19:23:41 +0200

    We implemented the previous application using React.

--- it-maza/src/App.tsx
+++ it-maza/src/App.tsx
@@ -1,5 +1,32 @@
+const tracks = [
+  {
+    id: 1,
+    title: "Vocal track",
+    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+  },
+  {
+    id: 2,
+    title: "Instrumental track",
+    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+  },
+];
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Music player</h1>
+      <ul>
+        {tracks.map((track) => {
+          return (
+            <li key={track.id}>
+              <div>{track.title}</div>
+              <audio controls src={track.url}></audio>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
